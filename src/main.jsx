b5d165e4943ebea1b03b13a4c6b01b7124f99d94
ZiadAhmed98 <72186630+ZiadAhmed98@users.noreply.github.com>
2026-05-21 02:06:11 +0400

    fixed DOM

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './styles/global.css';
+import { BrowserRouter } from 'react-router-dom'; // 1. Import this
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter> {/* 2. Wrap your entire App here */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
