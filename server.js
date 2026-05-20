935edb1efe493c6b33e05aabf057488b1de2ed47
ZiadAhmed98 <72186630+ZiadAhmed98@users.noreply.github.com>
2026-05-21 01:39:05 +0400

    fixing login v3

--- server.js
+++ server.js
@@ -231,4 +231,5 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// We add '0.0.0.0' so the container accepts outside traffic
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
