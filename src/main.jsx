f7c4d64f57d13c38599e7d01844a2276635509ab
ZiadAhmed98 <72186630+ZiadAhmed98@users.noreply.github.com>
2026-05-22 13:38:18 +0400

    Fixed the styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // 1. Import this
+import { BrowserRouter } from 'react-router-dom';
+
+// THIS IS THE MISSING LINK! 
+import './global.css'; 
+
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> {/* 2. Wrap your entire App here */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
